2166e5a304626cd0c95b4bade900e844dedfe192
Seb <seb@debian10pca1>
2020-10-02 15:10:40 +0200

    add install-op-rockpi/kicad/mp3server

--- projects/kicad/doc.mk
+++ projects/kicad/doc.mk
@@ -4,6 +4,7 @@
 ## \sa http://beaugrand.chez.com/
 ## \copyright CeCILL 2.1 Free Software license
 # ---------------------------------------------------------------------------- #
+TARDEPEND += kicad/*
 TARGETS += "| doc"
 TEXINPUTS = $(shell readlink -f $(PROROOT)/kicad):
 
