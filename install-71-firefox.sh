e5b12634eac3be13077e0b77591c7ad02eac35e0
Seb <seb@debian10pca1>
2020-10-16 15:20:46 +0200

    add install-op-firefox-cookies.sh

--- install-71-firefox.sh
+++ install-71-firefox.sh
@@ -19,12 +19,13 @@ fi
 
 pushd $home/.mozilla/firefox/*.default || return 1
 # Firefox telemetry and spy removal
-# https://gist.github.com/MrYar
-sed 's/US/FR/' $idir/install-*-firefox/install-*-firefox.js >user.js || return 1
+download https://gist.github.com/MrYar/751e0e5f3f1430db7ec5a8c8aa237b72/raw/6dba4f0d5eccac8691ad0b6b7f82287accaa719a/_Firefox-79 || return 1
+sed 's/US/FR/' $repo/_Firefox-79 >user.js || return 1
+sed -i -e '/autofillForms/d' -e '/rememberSignons/d' -e '/showSearch/d' user.js
 
-file=$idir/install-*-firefox/install-pr-firefox.html
+file=$idir/install-*-firefox/homepage-pr-.html
 if [ ! -f $file ]; then
-    file=`ls $idir/install-*-firefox/install-*-firefox.html`
+    file=`ls $idir/install-*-firefox/homepage.html`
 fi
 if [ -n "$file" ]; then
     cat >>user.js <<EOF
@@ -41,25 +42,38 @@ user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
 user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
 user_pref("browser.newtabpage.activity-stream.prerender", false);
 
-// Vider l'historique lors de la fermeture
-user_pref("privacy.sanitize.pending", "[{\"id\":\"shutdown\",\"itemsToClear\":[\"cache\",\"cookies\",\"offlineApps\",\"history\",\"formdata\",\"downloads\",\"sessions\",\"siteSettings\"],\"options\":{}}]");
-user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+user_pref("signon.autofillForms", true);
+user_pref("signon.rememberSignons", true);
+user_pref("browser.contentblocking.category", "custom");
+user_pref("network.cookie.cookieBehavior", 1);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
+user_pref("network.cookie.lifetimePolicy", 2);
+user_pref("accessibility.force_disabled", 1);
 
 // lockPref
 
 EOF
 chown $user.$user user.js
 file=`ls $idir/install-*-firefox/key4-pr-.db`
-[ -n $file ] && sudo -u $user cp $file key4.db
+if notFile key4.db; then
+    [ -n $file ] && sudo -u $user cp $file key4.db
+fi
 file=`ls $idir/install-*-firefox/logins-pr-.json`
-[ -n $file ] && sudo -u $user cp $file logins.json
+if notFile logins.json; then
+    [ -n $file ] && sudo -u $user cp $file logins.json
+fi
 popd
 
 dir=`ls -d $home/.mozilla/firefox/*.default-esr`
 if [ -n "$dir" ]; then
     pushd $dir || return 1
     sudo -u $user cp -a ../*.default/user.js .
-    sudo -u $user cp -a ../*.default/key4.db .
-    sudo -u $user cp -a ../*.default/logins.json .
+    if notFile key4.db; then
+        sudo -u $user cp -a ../*.default/key4.db .
+    fi
+    if notFile logins.json; then
+        sudo -u $user cp -a ../*.default/logins.json .
+    fi
     popd
 fi
---
+++ install-op-firefox-cookies.sh
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------------------------- #
+## \file install-op-firefox-cookies.sh
+## \author Sebastien Beaugrand
+## \sa http://beaugrand.chez.com/
+## \copyright CeCILL 2.1 Free Software license
+## \note https://addons.mozilla.org/fr/firefox/addon/i-dont-care-about-cookies/
+# ---------------------------------------------------------------------------- #
+sudo -u $user firefox https://addons.mozilla.org/firefox/downloads/latest/i-dont-care-about-cookies
