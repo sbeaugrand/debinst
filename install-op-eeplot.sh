678ca1cbc74dd8b1020f649241d67a52228972db
Seb <seb@debian10pca1>
2021-01-28 17:49:03 +0100

    eeplot: new repo

--- install-op-eeplot.sh
+++ install-op-eeplot.sh
@@ -5,32 +5,10 @@
 ## \copyright CeCILL 2.1 Free Software license
 # ---------------------------------------------------------------------------- #
 repo=$idir/../repo
-
-gitClone https://neo900.org/git/eeshow.git || return 1
-
-file=$bdir/eeshow/kicad/pl-render.c
-if notGrep "suppress_page_layout = 1" $file; then
-    pushd $bdir/eeshow || return 1
-    git apply $idir/install-op-eeplot.patch
-    popd
-fi
-
-file=$bdir/eeshow/.gitignore 
-if notFile $file; then
-    cat >$file <<EOF
-*.d
-*.o
-*.hex
-*.inc
-eediff
-eeplot
-eeshow
-eetest
-EOF
-fi
+gitClone https://github.com/sbeaugrand/eeshow || return 1
 
 if notWhich eeplot; then
-    pushd $bdir/eeshow || return 1
+    pushd $bdir/eeshow/eeshow || return 1
     make >>$log 2>&1
     make >>$log 2>&1 install
     popd
