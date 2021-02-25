4a636a12c794cc55eeabb108425d2aa7d74d790f
Seb <seb@debian10pca1>
2021-02-25 17:21:53 +0100

    firefox: replace soundfixer by automute

--- install-op-firefox-automute.sh
+++ install-op-firefox-automute.sh
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------------- #
-## \file install-op-firefox-soundfixer.sh
+## \file install-op-firefox-automute.sh
 ## \author Sebastien Beaugrand
 ## \sa http://beaugrand.chez.com/
 ## \copyright CeCILL 2.1 Free Software license
 # ---------------------------------------------------------------------------- #
-sudo -u $user firefox https://addons.mozilla.org/firefox/downloads/latest/soundfixer
+sudo -u $user firefox https://addons.mozilla.org/firefox/downloads/latest/auto-mute
