945995f432899bffd2c653f4330275de49b19e63
Seb <seb@debian10pca1>
2021-04-02 16:23:32 +0200

    mobian: wake up service for gnome-clocks

---
+++ mobian/clks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# ---------------------------------------------------------------------------- #
+## \file clks.py
+## \author Sebastien Beaugrand
+## \sa http://beaugrand.chez.com/
+## \copyright CeCILL 2.1 Free Software license
+# ---------------------------------------------------------------------------- #
+import json
+import sys
+
+DAYS = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat')
+
+for p in json.load(sys.stdin):
+    if p['active']:
+        days = ','.join([DAYS[d] for d in p['days']])
+        h = p['hour']
+        m = p['minute'] - 1
+        if m < 0:
+            m = 59
+            h -= 1
+        print('# {}'.format(p['name']))
+        print('OnCalendar={} {:02d}:{:02d}:58'.format(days, h, m))
---
+++ mobian/clks.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------- #
+## \file clks.sh
+## \author Sebastien Beaugrand
+## \sa http://beaugrand.chez.com/
+## \copyright CeCILL 2.1 Free Software license
+## \note Source: https://gitlab.gnome.org/kailueke/wake-mobile
+# ---------------------------------------------------------------------------- #
+path=`dirname $0`
+
+dir=/etc/systemd/system
+mkdir -p $dir/system-wake-up.timer.d
+
+file=$dir/system-wake-up.timer.d/10-mobian.conf
+echo "[Timer]" | sudo tee $file >/dev/null
+gsettings get org.gnome.clocks alarms |\
+ sed -e 's/[<>]//g' -e "s/'/\"/g" | $path/clks.py | sudo tee -a $file
+
+file=$dir/system-wake-up.timer
+[ -f $file ] || sudo tee $file >/dev/null <<EOF
+[Unit]
+Description=System Wake Up Timer
+[Timer]
+Persistent=false
+WakeSystem=true
+AccuracySec=1us
+OnCalendar=0000-01-01 00:00:00
+[Install]
+WantedBy=timers.target
+EOF
+
+file=$dir/system-wake-up.service
+[ -f $file ] || sudo tee $file >/dev/null <<EOF
+[Unit]
+Description=System Wake Up Action
+[Service]
+ExecStart=/usr/bin/true
+EOF
+
+sudo systemctl daemon-reload
+if ! systemctl -q is-enabled system-wake-up.timer; then
+    sudo systemctl enable system-wake-up.timer
+fi
+sudo systemctl restart system-wake-up.timer
+
+dir=~/.config/systemd/user
+mkdir -p $dir
+
+file=$dir/wake-up.timer
+cat >$file <<EOF
+[Unit]
+Description=User Wake Up Timer
+[Install]
+WantedBy=timers.target
+[Timer]
+Persistent=true
+AccuracySec=1us
+EOF
+gsettings get org.gnome.clocks alarms |\
+ sed -e 's/[<>]//g' -e "s/'/\"/g" | $path/clks.py >>$file
+
+file=~/.local/share/sounds/__custom/alarm-clock-elapsed.oga
+if [ -f $file ]; then
+    duration=`ffprobe -v error -show_entries format=duration\
+     -of default=noprint_wrappers=1:nokey=1 $file |\
+     awk '{ t=$0; if (t<60) { t=int(60/t)*t }; print int(t+1)+2 }'`
+else
+    duration=62
+fi
+echo "RuntimeMaxSec=$duration"
+
+file=$dir/wake-up.service
+cat >$file <<EOF
+[Unit]
+Description=User Wake Up Action
+[Service]
+ExecStart=/usr/bin/gnome-session-inhibit --inhibit suspend gnome-clocks
+RuntimeMaxSec=$duration
+EOF
+
+systemctl --user daemon-reload
+if ! systemctl --user -q is-enabled wake-up.timer; then
+    systemctl --user enable wake-up.timer
+fi
+systemctl --user restart wake-up.timer
