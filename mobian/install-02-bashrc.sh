8939f88d15c2f536f25a89cefb6d7c5cdc264bee
Seb <seb@debian10pca1>
2021-06-10 15:44:48 +0200

    mobian: mms

--- mobian/install-02-bashrc.sh
+++ mobian/install-02-bashrc.sh
@@ -15,6 +15,7 @@ alias ipf='sudo ip route add default via 10.66.0.2'
 alias ipt='sudo /usr/sbin/iptraf -i wwan0 -B -L /run/iptraf.log'
 alias ips='/home/mobian/install/debinst/mobian/ips.py'
 alias clks='/home/mobian/install/debinst/mobian/clks.sh'
+alias upg='cd /home/mobian/install/debinst/mobian && make upgrade'
 test -n "\$DISPLAY" && export GDK_BACKEND=x11
 EOF
 fi
---
+++ mobian/install-40-mmsd.sh
@@ -0,0 +1,95 @@
+# ---------------------------------------------------------------------------- #
+## \file install-40-mmsd.sh
+## \author Sebastien Beaugrand
+## \sa http://beaugrand.chez.com/
+## \copyright CeCILL 2.1 Free Software license
+# ---------------------------------------------------------------------------- #
+#MMSC=http://mms.free.fr
+MMSC=http://212.27.40.225
+APN=mmsfree
+
+# ---------------------------------------------------------------------------- #
+# mmsd
+# ---------------------------------------------------------------------------- #
+dir=$home/.mms/modemmanager
+if notDir $dir; then
+    mkdir -p $dir
+fi
+file=$dir/mms
+if notFile $file; then
+    cat >>$file <<EOF
+[Modem Manager]
+CarrierMMSC=$MMSC
+MMS_APN=$APN
+CarrierMMSProxy=NULL
+AutoProcessOnConnection=true
+AutoProcessSMSWAP=false
+EOF
+fi
+
+gitClone https://gitlab.com/kop316/mmsd.git || return 1
+
+if notWhich mmsdtng; then
+    pushd $bdir/mmsd || return 1
+    meson build
+    meson compile -C build
+    meson install -C build
+    popd
+fi
+
+file=/usr/lib/systemd/user/mmsd-tng.service
+if notFile $file; then
+    cat >>$file <<EOF
+[Unit]
+Description=Multimedia Messaging Service Daemon
+After=ModemManager.service
+
+[Service]
+ExecStart=/usr/local/bin/mmsdtng -d
+
+Restart=on-failure
+RestartSec=10s
+
+[Install]
+WantedBy=default.target
+EOF
+fi
+
+if ! sudo -u $user DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus XDG_RUNTIME_DIR=/run/user/1000 systemctl --user -q is-enabled mmsd-tng; then
+    sudo -u $user DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus XDG_RUNTIME_DIR=/run/user/1000 systemctl --user enable mmsd-tng
+fi
+sudo -u $user DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus XDG_RUNTIME_DIR=/run/user/1000 systemctl --user restart mmsd-tng
+
+# ---------------------------------------------------------------------------- #
+# purple-mm-sms
+# ---------------------------------------------------------------------------- #
+gitClone https://source.puri.sm/kop316/purple-mm-sms.git || return 1
+
+if notFile /usr/lib/purple-2/mm-sms.so; then
+    pushd $bdir/purple-mm-sms
+    make >>$log 2>&1
+    make >>$log 2>&1 install
+    popd
+fi
+
+# ---------------------------------------------------------------------------- #
+# chatty
+# ---------------------------------------------------------------------------- #
+gitClone https://source.puri.sm/Librem5/chatty.git || return 1
+
+if notFile /usr/local/bin/chatty; then
+    pushd $bdir/chatty
+    apt-get -y build-dep .
+    meson build
+    ninja -C build
+    ninja -C build install
+    popd
+fi
+
+# ---------------------------------------------------------------------------- #
+# APN
+# ---------------------------------------------------------------------------- #
+if ! nmcli c show FreeMMS >/dev/null 2>&1; then
+    nmcli c add type gsm con-name FreeMMS apn mmsfree gsm.home-only true gsm.username free gsm.password free || return 1
+    nmcli c up FreeMMS
+fi
