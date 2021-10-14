b25aa9968f97e46d3670d1de4d2b2916fcbe82e0
seb <seb@debian11pca1>
2021-10-14 14:44:33 +0200

    add install-14-pdfxup.sh (patch)

---
+++ install-14-pdfxup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------- #
+## \file install-14-pdfxup.sh
+## \author Sebastien Beaugrand
+## \sa http://beaugrand.chez.com/
+## \copyright CeCILL 2.1 Free Software license
+# ---------------------------------------------------------------------------- #
+pdfxup=/usr/bin/pdfxup
+if notGrep "2021" $pdfxup; then
+    file=$home/bin/pdfxup
+    if notFile $file; then
+        cp $pdfxup $file
+        patch -p0 $file >>$log <<EOF
+--- /usr/bin/pdfxup     2020-12-18 23:11:58.000000000 +0100
++++ $home/bin/pdfxup        2021-09-24 12:12:05.068126410 +0200
+@@ -453,7 +453,7 @@
+ ######################################################################
+ ## GS could be specified from command-line
+ : \${GS=\`which gs\`}
+-GSVERSION=\`\$GS --version 2>/dev/null\`;
++GSVERSION=\`\$GS --version 2>/dev/null | awk -F . '{ print \$1"."\$2 }'\`;
+ if [ ! "\$GSVERSION" ]; then
+     echo "   /!\\\\ ghostscript not found; aborting.";
+     exit 1;
+EOF
+    fi
+fi
