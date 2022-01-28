59845c419ac474b31a2394d79e65fe82d44428e4
seb <seb@debian11pca1>
2022-01-28 17:06:34 +0100

    add grbl-sim

---
+++ hardware/install-op-grbl-sim.sh
@@ -0,0 +1,24 @@
+# ---------------------------------------------------------------------------- #
+## \file install-op-grbl-sim.sh
+## \author Sebastien Beaugrand
+## \sa http://beaugrand.chez.com/
+## \copyright CeCILL 2.1 Free Software license
+# ---------------------------------------------------------------------------- #
+gitClone https://github.com/gnea/grbl.git || return 1
+gitClone https://github.com/sbeaugrand/grbl-sim.git || return 1
+
+dir=$bdir/grbl/grbl/grbl-sim
+if notDir $dir; then
+    cp -a $bdir/grbl-sim $dir
+fi
+
+if notFile $dir/gvalidate.exe; then
+    pushd $dir || return 1
+    make new
+    popd
+fi
+
+file=$home/bin/gvalidate
+if notFile $file; then
+    sudo -u $user cp $dir/gvalidate.exe $file
+fi
