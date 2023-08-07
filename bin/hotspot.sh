8f799343e9039cc1d8af6d3e3ad7932194f363d1
seb <seb@debian12pca1>
2023-08-07 10:45:20 +0200

    fix hotspot (2)

--- bin/hotspot.sh
+++ bin/hotspot.sh
@@ -7,16 +7,45 @@
 # ---------------------------------------------------------------------------- #
 quit()
 {
-    nmcli con down hotspot
-    sleep 1
+    if [ $1 = 0 ]; then
+        nmcli con down hotspot
+    fi
     /usr/sbin/rfkill block wifi
+    if [ $1 = 1 ]; then
+        exit 1
+    fi
+
+    enp=`sudo nft -a list chain filter FORWARD 2>/dev/null | grep 'iifname "enp' | awk '{ print $NF }'`
+    wlp=`sudo nft -a list chain filter FORWARD 2>/dev/null | grep 'iifname "wlp' | awk '{ print $NF }'`
+    if [ -n "$enp" ]; then
+        sudo nft delete rule filter FORWARD handle $enp
+    fi
+    if [ -n "$wlp" ]; then
+        sudo nft delete rule filter FORWARD handle $wlp
+    fi
+
     exit $1
 }
 
-sudo -k true || exit 1
 /usr/sbin/rfkill unblock wifi
-sleep 1
+sudo -K
+sudo true || quit 1
 nmcli con up hotspot || exit $?
+
+if sudo nft list chain filter FORWARD 2>/dev/null | grep -q docker; then
+    enp=`cat /proc/net/dev | cut -d ':' -f 1 | grep -m 1 '^enp'`
+    wlp=`cat /proc/net/dev | cut -d ':' -f 1 | grep -m 1 '^wlp'`
+    if [ -n "$enp" ] && [ -n "$wlp" ]; then
+        if ! sudo nft list chain filter FORWARD 2>/dev/null | grep -q "$wlp"; then
+            sudo nft add rule filter FORWARD iifname $wlp oifname $enp counter accept
+            sudo nft add rule filter FORWARD iifname $enp oifname $wlp ct state related,established counter accept
+        else
+            echo "warn: rule already exists"
+        fi
+    else
+        echo "warn: interface not found"
+    fi
+fi
 
 trap "echo; quit 0" SIGINT
 echo
