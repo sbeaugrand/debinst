1f2ca35c6b62263bbb004294888e6ee4f4e422cc
seb <seb@debian13pca1>
2026-05-14 21:33:26 +0200

    offline ansible manual

---
+++ install-op-/install-op-ansible-manual.sh
@@ -0,0 +1,14 @@
+# ---------------------------------------------------------------------------- #
+## \file install-op-ansible-manual.sh
+## \author Sebastien Beaugrand
+## \sa http://beaugrand.chez.com/
+## \copyright CeCILL 2.1 Free Software license
+# ---------------------------------------------------------------------------- #
+dir=$bdir/ansible-manual
+if notDir $dir; then
+    mkdir $dir
+    pushd $dir
+    $idir/bin/aspire.sh https://docs.ansible.com/projects/ansible/latest/collections/ansible/builtin/index.html
+    popd
+    logTodo "chromium file://$dir/index.html#modules"
+fi
